6c496770bd304944a7ca3e7e2593c5d1fe5a6b25
lokesh1008loki <Sherrylu118@gmail.com>
2025-04-29 20:15:57 +0530

    verce.config changes

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'standalone',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true
 }
 
 module.exports = nextConfig 
