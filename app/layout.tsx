e0ab76e4a2d3ccbc17e353e48f7bbfacb2a13dbc
lokesh1008loki <sherrylu118@gmail.com>
2025-07-20 00:18:38 +0530

    Add LinkedIn Insight Tag for conversion tracking and retargeting

--- app/layout.tsx
+++ app/layout.tsx
@@ -178,6 +178,31 @@ export default async function RootLayout({
             </>
           )}
         </ClientRoot>
+        
+        {/* LinkedIn Insight Tag */}
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `
+            _linkedin_partner_id = "7550706";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `
+        }} />
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]
+            }
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `
+        }} />
+        <noscript>
+          <img height="1" width="1" style={{display: "none"}} alt="" src="https://px.ads.linkedin.com/collect/?pid=7550706&fmt=gif" />
+        </noscript>
       </body>
     </html>
   )
