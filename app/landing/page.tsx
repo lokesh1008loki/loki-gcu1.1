41cc6959060d7df7ae584c549b7d43f6de9d8d1e
lokesh1008loki <lokeshbhati1008@gmail.com>
2026-05-28 00:36:27 +0530

    feat: add Roku Pixel Code to landing page

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -410,6 +410,19 @@ export default function MultiServiceLandingPage() {
           gtag('config', 'AW-18186338290');
         `}
       </Script>
+      <Script id="roku-pixel-landing" strategy="afterInteractive">
+        {`
+          !function(e,r){if(!e.rkp){var t=e.rkp=function(){
+          var e=Array.prototype.slice.call(arguments)
+          ;e.push(Date.now()),t.eventProcessor?t.eventProcessor.apply(t,e):t.queue.push(e)
+          };t.initiatorVersion="1.0",t.queue=[],t.load=function(e){
+          var t=r.createElement("script");t.async=!0,t.src=e
+          ;var n=r.getElementsByTagName("script")[0]
+          ;(n?n.parentNode:r.body).insertBefore(t,n)},rkp.load("https://cdn.ravm.tv/ust/dist/rkp.loader.js")}
+          }(window,document);
+          rkp("init","PaccB3hDqEZS"),rkp('event', 'PAGE_VIEW');
+        `}
+      </Script>
       <Toaster position="top-center" richColors />
 
       {/* Top Promotional Bar */}
